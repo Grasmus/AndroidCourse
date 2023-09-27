0d39639fbcc9f257cf6ccc1e6cdf98f7b24fb921
Grasmus <91031013+Grasmus@users.noreply.github.com>
2023-09-27 15:28:45 +0300

    Final commit

--- app/src/main/java/com/example/day4trafficlights/MainActivity.kt
+++ app/src/main/java/com/example/day4trafficlights/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.day4trafficlights
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rootLayout : ConstraintLayout = findViewById(R.id.root_layout)
+        val textViewTrafficLight : TextView = findViewById(R.id.textView_traffic_light)
+        val buttonRed : Button = findViewById(R.id.button_red)
+        val buttonYellow : Button = findViewById(R.id.button_yellow)
+        val buttonGreen : Button = findViewById(R.id.button_green)
+
+        buttonRed.setOnClickListener {
+            textViewTrafficLight.text = getString(R.string.button_red_text)
+            rootLayout.setBackgroundColor(getColor(R.color.red))
+        }
+
+        buttonYellow.setOnClickListener {
+            textViewTrafficLight.text = getString(R.string.button_yellow_text)
+            rootLayout.setBackgroundColor(getColor(R.color.yellow))
+        }
+
+        buttonGreen.setOnClickListener {
+            textViewTrafficLight.text = getString(R.string.button_green_text)
+            rootLayout.setBackgroundColor(getColor(R.color.green))
+        }
     }
 }
