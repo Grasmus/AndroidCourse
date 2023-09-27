12de5a16779f9770808b9512a42edfe1b01a5db8
Grasmus <91031013+Grasmus@users.noreply.github.com>
2023-09-27 10:10:45 +0300

    Final commit

--- app/src/main/java/com/example/day3crowscounter/MainActivity.kt
+++ app/src/main/java/com/example/day3crowscounter/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.day3crowscounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var counter : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSayHello : Button = findViewById(R.id.button_say_hello)
+        val buttonCrowsCounter : Button = findViewById(R.id.button_crows_counter)
+        val textViewMain : TextView = findViewById(R.id.textView_main)
+
+        buttonSayHello.setOnClickListener {
+            textViewMain.text = getString(R.string.say_hello_message)
+        }
+
+        buttonCrowsCounter.setOnClickListener {
+            textViewMain.text = String.format(getString(R.string.counter_message), ++counter)
+        }
     }
 }
