43ade4d593e6503e8bbd140e2ec0a95ecaaa0490
Grasmus <91031013+Grasmus@users.noreply.github.com>
2023-10-06 21:34:22 +0300

    Final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,12 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,7 +42,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.github.kirich1409:viewbindingpropertydelegate-full:1.5.9")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
